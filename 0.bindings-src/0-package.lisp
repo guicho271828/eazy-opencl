4be05e161f49855f57f2ded0f3ceb626f4287629
Masataro Asai <guicho2.71828@gmail.com>
2015-10-09 21:36:00 +0900

    export more symbols from bindings; share CL:FLOAT to avoid collision

--- 0.bindings-src/0-package.lisp
+++ 0.bindings-src/0-package.lisp
@@ -1,7 +1,10 @@
 
 (cl:defpackage #:eazy-opencl.bindings
   (:use :cffi)
+  (:import-from :cl :float)
   (:nicknames :%cl)
   (:export
-   #:*defined-opencl-functions*))
+   #:*defined-opencl-functions*
+   #:error-code
+   #:command-execution-status))
 
