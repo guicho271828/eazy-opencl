ba65be0845512aa5f200b3d4f0daccbdedb81e38
Masataro Asai <guicho2.71828@gmail.com>
2015-10-09 11:41:05 +0900

    :calling-convention is deprecated

--- 0.bindings-src/1.library.lisp
+++ 0.bindings-src/1.library.lisp
@@ -2,7 +2,7 @@
 
 (define-foreign-library opencl
   (:darwin (:framework "OpenCL")) ;; ?
-  (:windows "OpenCL.dll" :calling-convention :stdcall)
+  (:windows "OpenCL.dll" :convention :stdcall)
   (:unix (:or "libOpenCL.so" "libOpenCL.so.1" "libOpenCL.so.1.0" )))
 
 (use-foreign-library opencl)
